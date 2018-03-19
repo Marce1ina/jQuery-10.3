99ae239f8924ab37c044d6b7168aa64936023517
Marcelina Kucięba <marcelinakucieba@wp.pl>
2018-03-19 15:35:09 +0100

    Add navigation dots

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
 $(document).ready(function () {
 
-    carouselList = $("#carousel ul");
+    let carouselList = $("#carousel ul");
+    let navDots = $("#nav-dots");
 
     function moveFirstSlide() {
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
+        let firstItem = carouselList.find("li:first");
+        let lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({ marginLeft: 0 });
-    }
+    };
+    function moveLastSlide() {
+        let firstItem = carouselList.find("li:first");
+        let lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({ marginLeft: -940 });
+    };
 
-    function changeSlide() {
+    function moveLastDot() {
+        let firstDot = navDots.find("div:first");
+        let lastDot = navDots.find("div:last");
+        firstDot.before(lastDot);
+    };
+    function moveFirstDot() {
+        let firstDot = navDots.find("div:first");
+        let lastDot = navDots.find("div:last");
+        lastDot.after(firstDot);
+    };
+
+    function changeSlideNext() {
         carouselList.animate({ 'marginLeft': -940 }, 800, moveFirstSlide);
-    }
+        setTimeout(moveLastDot, 800);
+    };
+    function changeSlidePrev() {
+        moveLastSlide();
+        carouselList.animate({ 'marginLeft': 0 }, 800, );
+        setTimeout(moveFirstDot, 800);
+    };
 
-    setInterval(changeSlide, 4000);
+    setInterval(changeSlideNext, 4000);
 
     $("[class$='right']").on("click", function (event) {
-        changeSlide()
+        changeSlideNext();
     });
 
     $("[class$='left']").on("click", function (event) {
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        firstItem.before(lastItem);
-        carouselList.css({ marginLeft: -940 });
-        carouselList.animate({ 'marginLeft': 0 }, 800, );
+        changeSlidePrev();
     });
-
-
 
 });
