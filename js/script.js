af0cfac0ada53e7b0683f3ad3f6d3449509a4211
Marcelina Kucięba <marcelinakucieba@wp.pl>
2018-03-19 14:26:36 +0100

    Add navigation arrows

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-$(document).ready(function(){
-});
+$(document).ready(function () {
 
-setInterval(changeSlide, 4000);
-
-function changeSlide() {
     carouselList = $("#carousel ul");
-    carouselList.animate({'marginLeft':-940}, 800, moveFirstSlide);
-}
 
-function moveFirstSlide () {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-}
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({ marginLeft: 0 });
+    }
+
+    function changeSlide() {
+        carouselList.animate({ 'marginLeft': -940 }, 800, moveFirstSlide);
+    }
+
+    setInterval(changeSlide, 4000);
+
+    $("[class$='right']").on("click", function (event) {
+        changeSlide()
+    });
+
+    $("[class$='left']").on("click", function (event) {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({ marginLeft: -940 });
+        carouselList.animate({ 'marginLeft': 0 }, 800, );
+    });
+
+
+
+});
