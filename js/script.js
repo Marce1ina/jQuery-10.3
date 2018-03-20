2ae1d25d6301fe8371c13a90e8a045aadc82bdc2
Marcelina Kucięba <marcelinakucieba@wp.pl>
2018-03-20 22:55:37 +0100

    Add onclick functions for nav-dots

--- js/script.js
+++ js/script.js
@@ -3,48 +3,72 @@ $(document).ready(function () {
     let carouselList = $("#carousel ul");
     let navDots = $("#nav-dots");
 
-    function moveFirstSlide() {
-        let firstItem = carouselList.find("li:first");
-        let lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);
+    function moveLastDot(howMany) {
+        for (var i = 0; i < howMany; i++) {
+            let firstDot = navDots.find("div:first");
+            let lastDot = navDots.find("div:last");
+            firstDot.before(lastDot);
+        }
+    };
+
+    function moveFirstDot(howMany) {
+        for (var i = 0; i < howMany; i++) {
+            let firstDot = navDots.find("div:first");
+            let lastDot = navDots.find("div:last");
+            lastDot.after(firstDot);
+        }
+    };
+
+    function moveFirstSlides(howMany) {
+        for (var i = 0; i < howMany; i++) {
+            let firstItem = carouselList.find("li:first");
+            let lastItem = carouselList.find("li:last");
+            lastItem.after(firstItem);
+        }
+        moveLastDot(howMany);
         carouselList.css({ marginLeft: 0 });
     };
-    function moveLastSlide() {
-        let firstItem = carouselList.find("li:first");
-        let lastItem = carouselList.find("li:last");
-        firstItem.before(lastItem);
+    
+    function moveLastSlides(howMany) {
+        for (var i = 0; i < howMany; i++) {
+            let firstItem = carouselList.find("li:first");
+            let lastItem = carouselList.find("li:last");
+            firstItem.before(lastItem);
+        }
+        moveFirstDot(howMany);
         carouselList.css({ marginLeft: -940 });
     };
 
-    function moveLastDot() {
-        let firstDot = navDots.find("div:first");
-        let lastDot = navDots.find("div:last");
-        firstDot.before(lastDot);
-    };
-    function moveFirstDot() {
-        let firstDot = navDots.find("div:first");
-        let lastDot = navDots.find("div:last");
-        lastDot.after(firstDot);
+    function changeSlidesNext(howMany) {
+        carouselList.animate({ 'marginLeft': -940 * howMany }, 800, function () {
+            moveFirstSlides(howMany);
+        });
     };
 
-    function changeSlideNext() {
-        carouselList.animate({ 'marginLeft': -940 }, 800, moveFirstSlide);
-        setTimeout(moveLastDot, 800);
-    };
-    function changeSlidePrev() {
-        moveLastSlide();
+    function changeSlidesPrev(howMany) {
+        moveLastSlides(howMany);
         carouselList.animate({ 'marginLeft': 0 }, 800, );
-        setTimeout(moveFirstDot, 800);
     };
 
-    setInterval(changeSlideNext, 4000);
+    setInterval(function () {changeSlidesNext(1);}, 6000);
 
     $("[class$='right']").on("click", function (event) {
-        changeSlideNext();
+        changeSlidesNext(1);
     });
 
     $("[class$='left']").on("click", function (event) {
-        changeSlidePrev();
+        changeSlidesPrev(1);
+    });
+
+    $(".dot").on("click", function (event) {
+        var dotIndex = $(".dot").index(this);
+        var activeDotIndex = $(".dot").index($(".dot-active"));
+        if (dotIndex < activeDotIndex) {
+            changeSlidesPrev(activeDotIndex - dotIndex);
+        }
+        else if (dotIndex > activeDotIndex) {
+            changeSlidesNext(dotIndex - activeDotIndex);
+        };
     });
 
 });
